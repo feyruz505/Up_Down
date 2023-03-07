2098f52bda1ee9b3b77237d8fb446ce010a67ce6
Elekb_yf16 <Elekb_yf16@STHQ012E-16>
2023-03-07 10:47:31 +0400

    Add project files.

---
+++ Up_Down/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Up_Down
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Up_Click(object sender, RoutedEventArgs e)
+        {
+            scrollBar.Value += 0.1;
+        }
+
+        private void Down_Click(object sender, RoutedEventArgs e)
+        {
+            scrollBar.Value -= 0.1;
+        }
+
+
+
+
+
+    }
+}
